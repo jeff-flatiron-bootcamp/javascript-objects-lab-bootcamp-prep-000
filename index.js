8686f0aeb53ca92fd443552344d2e7ea2a0afc9b
Jeff Simon <jeffsbillinfo@gmail.com>
2020-02-11 05:00:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,14 +18,21 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 
 function deleteFromObjectByKey(object, key)
 {
+<<<<<<< HEAD
   var returnObject = Object.assign({},object);
+=======
+  var returnObject = object;
+>>>>>>> 7b80d63229b5d0a51d80dc257b2dd452658a7219
   delete returnObject[key];
   return returnObject;
 }
 
+<<<<<<< HEAD
 
 function destructivelyDeleteFromObjectByKey(object, key)
 {
   delete object[key];
   return object;
 }
+=======
+>>>>>>> 7b80d63229b5d0a51d80dc257b2dd452658a7219
